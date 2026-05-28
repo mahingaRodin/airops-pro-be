7fb79410c13bbf78c71b3dcc9edb3e019d288afa
P-Odiix <mahingarodin@gmail.com>
2026-05-28 14:09:25 +0200

    feat: add location-service microservice implementation

--- services/location-service/src/main/java/com/stack/location_service/controller/HomeController.java
+++ services/location-service/src/main/java/com/stack/location_service/controller/HomeController.java
@@ -1,4 +1,16 @@
 package com.stack.location_service.controller;
 
+
+import com.stack.payloads.responses.ApiResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+    @GetMapping()
+    public ApiResponse HomeController() {
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setMessage("Hello This is the Location Service At Work!");
+        return apiResponse;
+    }
 }
